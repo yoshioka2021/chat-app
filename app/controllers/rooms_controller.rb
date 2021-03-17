851f440b709131b6da3469c02ea0a50663034164
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-03-17 21:57:50 +0900

    メッセージの表示

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,7 +9,7 @@ class RoomsController < ApplicationController
   def create
     @room = Room.new(room_params)
     if @room.save
-      redirect_to rooms_path
+      redirect_to root_path
     else
       render :new
     end
