3fcb6ee2e0c53694e1df1ab6a27ca78b1e79c628
yoshioka2021 <80379761+yoshioka2021@users.noreply.github.com>
2021-03-19 12:37:41 +0900

    結合テストの実装

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,6 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 2021_03_18_074857) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
